e1bed22bf664f7f1d4a103cb3d7593d540ae1c17
Aleksandra Ryyberg <aleksandra.ryyberg@mail.ee>
2025-05-13 16:49:03 +0300

    viimane_loeng

--- app.js
+++ app.js
@@ -3,6 +3,52 @@ const menuPanel = document.querySelector('.menu-panel');
 const menuLinks = document.querySelectorAll('.menu-panel a, .menu-panel button');
 const frameImages = document.querySelector('#frame');
 const images = ['2.webp', '1-1.webp', '2-2.webp', '3-3.webp', '4-4.webp','5-5.webp'];
+const modal = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('#btn-open-form');
+const btnCloseModal = document.querySelector('#modal-close-button');
+
+
+/*btnOpenModal.addEventListener('click', () => {
+    modal.style.display = 'block';
+    document.body.classList.add('no-scroll'); 
+});
+
+
+btnCloseModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    document.body.classList.remove('no-scroll'); 
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+        document.body.classList.remove('no-scroll');
+    }
+}); */
+
+btnOpenModal.addEventListener('click', () => {
+    modal.classList.add('show');
+    document.body.classList.add('no-scroll');
+});
+
+btnCloseModal.addEventListener('click', () => {
+    modal.classList.remove('show');
+    document.body.classList.remove('no-scroll');
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.classList.remove('show');
+        document.body.classList.remove('no-scroll');
+    }
+});
+
+const closeBtn = document.querySelector('.close-button');
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
 
 let currentImageIndex = 0;
 
